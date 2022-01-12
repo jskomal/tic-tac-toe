ca32b77a49b28bde6e283f15ab77fc7c9d1f0d71
Jordan Skomal <jskomal@gmail.com>
2022-01-11 19:33:31 -0700

    Add player.js

---
+++ player.js
@@ -0,0 +1,12 @@
+class Player {
+  constructor() {
+    this.id = 'one';
+    this.token = 'titan';
+    this.wins = 0;
+  }
+
+  becomePlayerTwo() {
+    this.id = 'two'
+    this.token = 'warlock'
+  }
+}
