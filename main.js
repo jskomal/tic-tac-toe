9f7c2b9e1ade0e5ba0ef2527b23758436c55b07d
Jordan Skomal <jskomal@gmail.com>
2022-01-17 22:11:15 -0700

    Refactor assets and handle multiple clicks on the same tile

--- main.js
+++ main.js
@@ -9,39 +9,43 @@ var endText = document.querySelector('#endText');
 // data
 var game = new Game();
 
-
 // event listeners
-window.addEventListener('load', displayTurn)
+window.addEventListener('load', displayTurn);
 
 gameTiles.forEach(function (element) {
-  element.addEventListener('click', gameFlow)
-})
-
+  element.addEventListener('click', gameFlow, {once:true});
+});
 
 // functions
 function displayTurn() {
-  turnIndicator.innerText = `${game.currentPlayer}, it's your turn!`
+  turnIndicator.innerText = `${game.currentPlayer}, it's your turn!`;
 }
 
 function displayToken(e) {
   targetTile = e.target;
-  targetTile.classList.add((game.currentPlayer === 'titan' ? 'placed-token-warlock' : 'placed-token-titan'))
+  targetTile.classList.add(
+    game.currentPlayer === 'titan'
+      ? 'placed-token-warlock'
+      : 'placed-token-titan'
+  );
 }
 
 function updateScore() {
-winsCounterTitan.innerText = game.titan.wins;
-winsCounterWarlock.innerText = game.warlock.wins;
+  winsCounterTitan.innerText = game.titan.wins;
+  winsCounterWarlock.innerText = game.warlock.wins;
 }
 
 function checkForEnd() {
   if (game.isWon) {
-    endScreen.classList.remove('hidden')
-    endText.innerText = `Congratulations, ${game.currentPlayer === 'titan' ? 'Warlock' : 'Titan'}, 
+    endScreen.classList.remove('hidden');
+    endText.innerText = `Congratulations, ${
+      game.currentPlayer === 'titan' ? 'Warlock' : 'Titan'
+    }, 
       You've won!
-      Let's go again!`
+      Let's go again!`;
     restart();
   } else if (game.isDraw) {
-    endScreen.classList.remove('hidden'); 
+    endScreen.classList.remove('hidden');
     endText.innerText = `No winner this time, 
     Let's go again!`;
     restart();
@@ -49,12 +53,20 @@ function checkForEnd() {
 }
 
 function restart() {
-  gameTiles.forEach(element => {
+  gameTiles.forEach((element) => {
     element.classList.remove('placed-token-warlock');
     element.classList.remove('placed-token-titan');
-  })
-  game.restartGame()
-  setTimeout(() => {endScreen.classList.add('hidden')}, 2500)
+  });
+  game.restartGame();
+  setTimeout(() => {
+    endScreen.classList.add('hidden');
+  }, 2500);
+  gameTiles.forEach(function (element) {
+    element.removeEventListener('click', gameFlow, { once: true });
+  });
+  gameTiles.forEach(function (element) {
+    element.addEventListener('click', gameFlow, { once: true });
+  });
 }
 
 function gameFlow(e) {
@@ -64,4 +76,3 @@ function gameFlow(e) {
   checkForEnd();
   displayTurn();
 }
-
